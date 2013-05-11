4621da7229c28052d8f7906b0d224ad402214af6
zephyrz4 <zephyrz2712@gmail.com>
2013-05-11 15:22:29 -0400

    Added SGame

---
+++ src/SGame.cpp
@@ -0,0 +1,18 @@
+/*
+ * SGame.cpp
+ *
+ *  Created on: May 11, 2013
+ *      Author: Cam
+ */
+
+#include "SGame.hpp"
+
+SGame::SGame() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SGame::~SGame() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/SGame.hpp
@@ -0,0 +1,17 @@
+/*
+ * SGame.hpp
+ *
+ *  Created on: May 11, 2013
+ *      Author: Cam
+ */
+
+#ifndef SGAME_HPP_
+#define SGAME_HPP_
+
+class SGame {
+public:
+	SGame();
+	virtual ~SGame();
+};
+
+#endif /* SGAME_HPP_ */
