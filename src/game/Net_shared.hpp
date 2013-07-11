3b28b609234748dbff80eb55e7226fd871854126
Cameron Morris <zephyrz2712@gmail.com>
2013-07-11 15:34:06 -0400

    Some stuff to start on networking

---
+++ src/game/Net_shared.hpp
@@ -0,0 +1,24 @@
+/*
+ * Net_shared.hpp
+ *
+ *  Created on: Jul 11, 2013
+ *      Author: cameron
+ */
+
+#ifndef NET_SHARED_HPP_
+#define NET_SHARED_HPP_
+
+enum PacketTypes{
+
+	CONNECT,
+	DISCONNECT,
+	CLIENT_UPDATE,
+	SERVER_UPDATE,
+
+};
+
+
+
+
+
+#endif /* NET_SHARED_HPP_ */
