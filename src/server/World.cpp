47063ca942a5433112f84bc3e0b9c8b3bc21109f
zephyrz4 <zephyrz2712@gmail.com>
2013-08-01 19:30:22 -0400

    Added world class

---
+++ src/server/World.cpp
@@ -0,0 +1,18 @@
+/*
+ * World.cpp
+ *
+ *  Created on: Aug 1, 2013
+ *      Author: Cam
+ */
+
+#include "World.hpp"
+
+World::World() {
+	// TODO Auto-generated constructor stub
+
+}
+
+World::~World() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/server/World.hpp
@@ -0,0 +1,22 @@
+/*
+ * World.hpp
+ *
+ *  Created on: Aug 1, 2013
+ *      Author: Cam
+ */
+
+#ifndef WORLD_HPP_
+#define WORLD_HPP_
+
+#define WORLD_HEIGHT 800
+#define WORLD_WIDTH 600
+
+class World {
+public:
+	World();
+	virtual ~World();
+private:
+
+};
+
+#endif /* WORLD_HPP_ */
