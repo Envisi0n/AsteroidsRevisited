e22d5675b5ffe92dc59b2f9d826d976a35032858
zephyrz4 <zephyrz2712@gmail.com>
2013-08-02 12:54:15 -0400

    Oops

---
+++ src/shared/GameGlobals.hpp
@@ -0,0 +1,14 @@
+/*
+ * GameGlobals.hpp
+ *
+ *  Created on: Aug 2, 2013
+ *      Author: Cam
+ */
+
+#ifndef GAMEGLOBALS_HPP_
+#define GAMEGLOBALS_HPP_
+
+#define WORLD_HEIGHT 800
+#define WORLD_WIDTH 600
+
+#endif /* GAMEGLOBALS_HPP_ */
