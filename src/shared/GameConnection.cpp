169a71a2f0349f805237fac26fa2be33556da296
zephyrz4 <zephyrz2712@gmail.com>
2013-08-20 14:28:36 -0400

    Some more work on networking

--- src/shared/GameConnection.cpp
+++ src/shared/GameConnection.cpp
@@ -7,12 +7,88 @@
 
 #include "GameConnection.hpp"
 
-GameConnection::GameConnection() {
-	// TODO Auto-generated constructor stub
+GameConnection::GameConnection(unsigned int protocolId, float timeout) {
+
+	setProtocolId(protocolId);
+	setTimeout(timeout);
+	setMode(None);
+	setRunning(false);
+	ClearData();
 
 }
 
 GameConnection::~GameConnection() {
-	// TODO Auto-generated destructor stub
+
 }
 
+sf::IpAddress GameConnection::getAddress() const {
+	return address;
+}
+
+void GameConnection::setAddress(sf::IpAddress address) {
+	this->address = address;
+}
+
+Mode GameConnection::getMode() const {
+	return mode;
+}
+
+void GameConnection::setMode(Mode mode) {
+	this->mode = mode;
+}
+
+int GameConnection::getPort() const {
+	return port;
+}
+
+void GameConnection::setPort(int port) {
+	this->port = port;
+}
+
+unsigned int GameConnection::getProtocolId() const {
+	return protocolId;
+}
+
+void GameConnection::setProtocolId(unsigned int protocolId) {
+	this->protocolId = protocolId;
+}
+
+bool GameConnection::isRunning() const {
+	return running;
+}
+
+void GameConnection::setRunning(bool running) {
+	this->running = running;
+}
+
+State GameConnection::getState() const {
+	return state;
+}
+
+void GameConnection::setState(State state) {
+	this->state = state;
+}
+
+float GameConnection::getTimeout() const {
+	return timeout;
+}
+
+void GameConnection::setTimeout(float timeout) {
+	this->timeout = timeout;
+}
+
+float GameConnection::getTimeoutAccumulator() const {
+	return timeoutAccumulator;
+}
+
+void GameConnection::setTimeoutAccumulator(float timeoutAccumulator) {
+	this->timeoutAccumulator = timeoutAccumulator;
+}
+
+void GameConnection::ClearData() {
+
+	setState(Disconnected);
+	setTimeoutAccumulator(0.0f);
+	setAddress(address.None);
+	setPort(0);
+}
--- src/shared/GameConnection.hpp
+++ src/shared/GameConnection.hpp
@@ -10,20 +10,40 @@
 
 #include <SFML/Network.hpp>
 
+enum Mode {
+	None, Client, Server
+};
+
+enum State {
+	Disconnected, Listening, Connecting, ConnectFail, Connected
+};
+
 class GameConnection {
 public:
 
-	enum Mode {
-		None, Client, Server
-	};
-
-	GameConnection();
+	GameConnection(unsigned int protocolId, float timeout);
 	virtual ~GameConnection();
 
+	sf::IpAddress getAddress() const;
+	void setAddress(sf::IpAddress address);
+	Mode getMode() const;
+	void setMode(Mode mode);
+	int getPort() const;
+	void setPort(int port);
+	unsigned int getProtocolId() const;
+	void setProtocolId(unsigned int protocolId);
+	bool isRunning() const;
+	void setRunning(bool running);
+	State getState() const;
+	void setState(State state);
+	float getTimeout() const;
+	void setTimeout(float timeout);
+	float getTimeoutAccumulator() const;
+	void setTimeoutAccumulator(float timeoutAccumulator);
+
 private:
-	enum State {
-		Disconnected, Listening, Connecting, ConnectFail, Connected
-	};
+
+	void ClearData();
 
 	unsigned int protocolId;
 	float timeout;
