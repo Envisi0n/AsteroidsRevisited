5e0d388ef1789af8e78c3bad00799c236302d707
zephyrz4 <zephyrz2712@gmail.com>
2013-08-20 21:51:09 -0400

    Some more work on the new networking

--- src/shared/GameConnection.cpp
+++ src/shared/GameConnection.cpp
@@ -6,6 +6,9 @@
  */
 
 #include "GameConnection.hpp"
+#include <iostream>
+
+const int headerSize = 16;
 
 GameConnection::GameConnection(unsigned int protocolId, float timeout) {
 
@@ -18,6 +21,125 @@ GameConnection::GameConnection(unsigned int protocolId, float timeout) {
 }
 
 GameConnection::~GameConnection() {
+
+	if (isRunning())
+		Stop();
+
+}
+
+bool GameConnection::Start(int port) {
+
+	std::cout << "start connection on port " << port << std::endl;
+
+	if (!socket.bind(port))
+		return false;
+	setRunning(true);
+	OnStart();
+	return true;
+
+}
+
+void GameConnection::Stop() {
+
+	std::cout << "stop connection" << std::endl;
+	ClearData();
+	socket.unbind();
+	running = false;
+	if (isConnected())
+		OnDisconnect();
+	OnStop();
+
+}
+
+void GameConnection::Listen() {
+
+	std::cout << "server listening for connection" << std::endl;
+	ClearData();
+	if (isConnected())
+		OnDisconnect();
+	setMode(Server);
+	setState(Listening);
+}
+
+void GameConnection::Connect(sf::IpAddress address) {
+
+	std::cout << "client connecting to " << address.toString() << std::endl;
+	ClearData();
+	if (isConnected())
+		OnDisconnect();
+	setMode(Client);
+	setState(Connecting);
+	setAddress(address);
+
+}
+
+void GameConnection::Update(float deltaTime) {
+
+	timeoutAccumulator += deltaTime;
+	if (timeoutAccumulator > timeout) {
+		if (isConnecting()) {
+			std::cout << "connect timed out" << std::endl;
+			ClearData();
+			setState(ConnectFail);
+			OnDisconnect();
+		} else if (isConnected()) {
+			std::cout << "connection timed out\n" << std::endl;
+			ClearData();
+			if (isConnecting()) {
+				setState(ConnectFail);
+			}
+			OnDisconnect();
+		}
+	}
+
+	reliabilitySystem.Update(deltaTime);
+
+}
+
+bool GameConnection::SendPacket(sf::Packet packet) {
+
+	sf::Packet gamePacket;
+
+	if (address == address.None)
+		return false;
+
+	// Write packet header
+	gamePacket << getProtocolId();
+	gamePacket << reliabilitySystem.GetLocalSequence();
+	gamePacket << reliabilitySystem.GetRemoteSequence();
+	gamePacket << reliabilitySystem.GenerateAckBits();
+
+	// Copy data into packet
+	gamePacket << packet.getData();
+
+	socket.send(gamePacket, getAddress(), getPort());
+
+	reliabilitySystem.PacketSent(gamePacket.getDataSize());
+	return true;
+
+}
+
+bool GameConnection::SendPacket(sf::Packet packet, sf::IpAddress ipAddress,
+		int port) {
+
+	sf::Packet gamePacket;
+
+	if (address == address.None)
+		return false;
+
+	// Write packet header
+	gamePacket << getProtocolId();
+	gamePacket << reliabilitySystem.GetLocalSequence();
+	gamePacket << reliabilitySystem.GetRemoteSequence();
+	gamePacket << reliabilitySystem.GenerateAckBits();
+
+	// Copy data into packet
+	gamePacket << packet.getData();
+
+	socket.send(gamePacket, ipAddress, port);
+
+	reliabilitySystem.PacketSent(gamePacket.getDataSize());
+	return true;
 
 }
 
@@ -83,6 +205,22 @@ float GameConnection::getTimeoutAccumulator() const {
 
 void GameConnection::setTimeoutAccumulator(float timeoutAccumulator) {
 	this->timeoutAccumulator = timeoutAccumulator;
+}
+
+bool GameConnection::isConnected() const {
+	return getState() == Connected;
+}
+
+bool GameConnection::isConnecting() const {
+	return getState() == Connecting;
+}
+
+bool GameConnection::isConnectFailed() const {
+	return getState() == ConnectFail;
+}
+
+bool GameConnection::isListening() const {
+	return getState() == Listening;
 }
 
 void GameConnection::ClearData() {
--- src/shared/GameConnection.hpp
+++ src/shared/GameConnection.hpp
@@ -9,6 +9,7 @@
 #define GAMECONNECTION_HPP_
 
 #include <SFML/Network.hpp>
+#include "GameReliabilitySystem.hpp"
 
 enum Mode {
 	None, Client, Server
@@ -24,6 +25,22 @@ public:
 	GameConnection(unsigned int protocolId, float timeout);
 	virtual ~GameConnection();
 
+	// Operations
+	bool Start(int port);
+	void Stop();
+	void Listen();
+	void Connect(sf::IpAddress address);
+	virtual void Update(float deltaTime);
+	virtual bool SendPacket(sf::Packet packet);
+	virtual bool SendPacket(sf::Packet packet, sf::IpAddress, int port);
+	// Utility
+
+	bool isConnected() const;
+	bool isConnecting() const;
+	bool isConnectFailed() const;
+	bool isListening() const;
+
+	// Getters/Setters
 	sf::IpAddress getAddress() const;
 	void setAddress(sf::IpAddress address);
 	Mode getMode() const;
@@ -41,6 +58,43 @@ public:
 	float getTimeoutAccumulator() const;
 	void setTimeoutAccumulator(float timeoutAccumulator);
 
+protected:
+	virtual void OnStart() {
+	}
+	virtual void OnStop() {
+	}
+	virtual void OnConnect() {
+	}
+	virtual void OnDisconnect() {
+	}
+
+	void WriteInteger(unsigned char * data, unsigned int value) {
+		data[0] = (unsigned char) (value >> 24);
+		data[1] = (unsigned char) ((value >> 16) & 0xFF);
+		data[2] = (unsigned char) ((value >> 8) & 0xFF);
+		data[3] = (unsigned char) (value & 0xFF);
+	}
+
+	void WriteHeader(unsigned char * header, unsigned int protocol, unsigned int sequence,
+			unsigned int ack, unsigned int ack_bits) {
+		WriteInteger(header, protocol);
+		WriteInteger(header + 4, sequence);
+		WriteInteger(header + 8, ack);
+		WriteInteger(header + 12, ack_bits);
+	}
+
+	void ReadInteger(const unsigned char * data, unsigned int & value) {
+		value = (((unsigned int) data[0] << 24) | ((unsigned int) data[1] << 16)
+				| ((unsigned int) data[2] << 8) | ((unsigned int) data[3]));
+	}
+
+	void ReadHeader(const unsigned char * header, unsigned int & sequence,
+			unsigned int & ack, unsigned int & ack_bits) {
+		ReadInteger(header, sequence);
+		ReadInteger(header + 4, ack);
+		ReadInteger(header + 8, ack_bits);
+	}
+
 private:
 
 	void ClearData();
@@ -55,6 +109,8 @@ private:
 	float timeoutAccumulator;
 	sf::IpAddress address;
 	int port;
+
+	GameReliabilitySystem reliabilitySystem;
 };
 
 #endif /* GAMECONNECTION_HPP_ */
